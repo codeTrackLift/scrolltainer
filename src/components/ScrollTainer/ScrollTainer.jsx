8d73bf79b9dbe8a58f7f363215925946a55bab1b
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-07-09 10:12:02 -0400

    css refactor, imageArray refactor

--- src/components/ScrollTainer/ScrollTainer.jsx
+++ src/components/ScrollTainer/ScrollTainer.jsx
@@ -1,31 +1,7 @@
 import { useEffect } from "react";
 import { motion } from 'framer-motion';
+import imageArray from '../../context/imageArray'
 import './ScrollTainer.css';
-import cameroRed from '../../images/cameroRed.jpg'
-import cameroYellow from '../../images/cameroYellow.jpg'
-import cobraGray from '../../images/cobraGray.jpg'
-import lamboOrange from '../../images/lamboOrange.jpg'
-import lamboWhite from '../../images/lamboWhite.jpg'
-import m4Black from '../../images/m4Black.jpg'
-import mercSLR from '../../images/mercSLR.jpg'
-import mustangWhite from '../../images/mustangWhite.jpg'
-import r8Fall from '../../images/r8Fall.jpg'
-import r8Track from '../../images/r8Track.jpg'
-import r8Red from '../../images/r8Red.jpg'
-
-const imageArray = [
-  cameroRed,
-  cameroYellow,
-  cobraGray,
-  lamboOrange,
-  lamboWhite,
-  m4Black,
-  mercSLR,
-  mustangWhite,
-  r8Fall,
-  r8Red,
-  r8Track,
-]
 
 const renderImages = ({height}) => {
   const images = imageArray.map((image, i) => {
