efb6c8497ae3b9b5eb4c9b7d83654720598b147b
Pete Chu <peterc0523@gmail.com>
2022-07-11 09:35:48 -0400

    better modal transition

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ const dropIn = {
     y: '0',
     opacity: 1,
     transition: {
-      duration: 0.1,
+      duration: 0.25,
       type: 'spring',
       damping: 25,
       stiffness: 500,
@@ -20,7 +20,10 @@ const dropIn = {
   },
   exit: {
     y: '100vh',
-    opacity: 0
+    opacity: 0, 
+    transition: {
+      duration: 0.25
+    }
   }
 };
 
